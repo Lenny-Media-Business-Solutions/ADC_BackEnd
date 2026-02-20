39ad5f759df98ed6386fcd6b6bf5bb49761130c6
mbogo756 <mbogo756@gmail.com>
2026-02-20 12:43:29 +0300

    Committed all changes made even in settings.py and in data.json

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
@@ -117,7 +117,8 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", ""),
         "PORT": os.environ.get("DB_PORT", 5432),
-        "OPTIONS": {"sslmode": "require"},
+        **({"OPTIONS": {"sslmode": "require"}} if not DEBUG else {})
+        # "OPTIONS": {"sslmode": "require"},
     }
 }
 
